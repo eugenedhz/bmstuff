73de306c948721cd54dd831c34ed9470727dfc81
eugenedzh <toropkibn@gmail.com>
2023-11-05 13:39:14 +0300

    Updated: api/routes/todos/controller.py; api/models.py

--- api/routes/todos/controller.py
+++ api/routes/todos/controller.py
@@ -12,7 +12,6 @@ from api.routes.todos.todos_to_json import todo_json
 @app.route('/todo', methods=['POST'])
 @jwt_required()
 def make_todo():
-
     json = request.json
     TodoSchema().validate(json)
 
@@ -27,13 +26,51 @@ def make_todo():
     return todo_json(todo, is_many=False, field_list=None), 201
 
 
-@app.route('/todo/<int:id>', methods=['PATCH'])
+@app.route('/todo', methods=['GET'])
+@jwt_required()
+def get_todos():
+    user_id = int(get_jwt_identity())
+
+    todos = Todo.query.filter_by(found_todo.user.id=user_id).all()
+
+    if len(todos) == 0:
+        return {'message': 'NO_TODOS'}, 200
+
+    return todo_json(todos, is_many=True, field_list=None), 200
+
+
+@app.route('/todo/<int:todo_id>', methods=['GET'])
+@jwt_required()
+def get_todo(todo_id):
+    user_id = int(get_jwt_identity())
+
+    todo = Todo.query.filter_by(todo.user.id=user_id, id=todo_id).first()
+
+    if todo is None:
+        raise ApiError('TODO_NOT_FOUND', status_code=409)
+
+    return todo_json(todo, is_many=False, field_list=None), 200
+
+
+@app.route('/todo/<int:todo_id>', methods=['PATCH'])
 @jwt_required()
 def update_todo(todo_id):
-    user_id = get_jwt_identity()
+    user_id = int(get_jwt_identity())
 
-    
+    todo = Todo.query.filter_by(todo.user.id=user_id, id=todo_id).first()
 
+    if found_todo is None:
+        raise ApiError('TODO_NOT_FOUND', status_code=409)
+
+    json = request.json
+    TodoSchemaUpdate().validate(json)
+
+
+    Todo.query.filter_by(id=id).update(json)
     db.session.commit()
 
-    return todo_json(todo, is_many=False, field_list=None), 201
+    updated_todo = Todo.query.filter_by(id=todo_id).first()
+
+    return todo_json(updated_todo, is_many=False, field_list=None), 200
+
+
